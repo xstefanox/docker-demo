63770454ab3a28f287734ba0668ebdbb5f7f4a8e
Stefano Varesi <stefano.varesi@gmail.com>
2016-03-20 12:24:58 +0100

    added nginx configuration file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World!<br><br>' + new Date());
 });
 
 app.listen(3000, function () {
